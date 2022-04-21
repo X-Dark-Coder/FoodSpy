f6e7522fcfb9f020ba9a5938416f69256a2eb7f5
X-Dark-Coder <marzban.dev@gmail.com>
2022-04-21 11:19:56 +0430

    Create button component

--- src/components/shared/Button/button.tsx
+++ src/components/shared/Button/button.tsx
@@ -15,14 +15,16 @@ const Button: React.FC<ButtonProps> = ({
     fullWidth,
     ...rest
 }) => {
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
     const buttonClasses = {
         base: `text-large font-semibold rounded-lg flex justify-center items-center 
         transition duration-100`,
-        "wide-primary": `w-[335px] h-[52px] bg-primary text-white hover:bg-primary-tint-10 active:scale-[0.98]`,
-        "wide-primary-white": `w-[335px] h-[52px] bg-white text-primary hover:bg-primary-tint-40 active:scale-[0.98]`,
-        "wide-secondary-white": `w-[335px] h-[52px] border-2 border-white text-white hover:bg-primary-tint-40 active:scale-[0.98]
+        "wide-primary": `w-[335px] h-[52px] bg-primary text-white hover:bg-primary-tint-10`,
+        "wide-primary-white": `w-[335px] h-[52px] bg-white text-primary hover:bg-primary-tint-40`,
+        "wide-secondary-white": `w-[335px] h-[52px] border-2 border-white text-white hover:bg-primary-tint-40
         hover:text-primary hover:border-primary-tint-40`,
-        "wide-secondary": `w-[335px] h-[52px] border-2 border-primary text-primary hover:bg-primary-tint-10 active:scale-[0.98]
+        "wide-secondary": `w-[335px] h-[52px] border-2 border-primary text-primary hover:bg-primary-tint-10
         hover:text-white hover:border-primary-tint-10`,
         "medium-1-primary": "",
         "medium-1-secondary": "",
@@ -34,8 +36,8 @@ const Button: React.FC<ButtonProps> = ({
         "small-white": "",
         "extra-small-primary": "",
         "extra-small-icon": "",
-        "expand-small": "w-[28px] h-[28px] rounded-lg bg-primary hover:bg-primary-tint-10 active:scale-[0.98]",
-        "expand-medium": "w-[36px] h-[36px] rounded-lg bg-primary hover:bg-primary-tint-10 active:scale-[0.98]",
+        "expand-small": "w-[28px] h-[28px] rounded-lg bg-primary hover:bg-primary-tint-10",
+        "expand-medium": "w-[36px] h-[36px] rounded-lg bg-primary hover:bg-primary-tint-10",
         tab: "",
         payment: "",
         disabled: "bg-mono-sky-lighter text-white",
@@ -45,11 +47,22 @@ const Button: React.FC<ButtonProps> = ({
         buttonClasses.base,
         disabled ? buttonClasses.disabled : buttonClasses[variant],
         fullWidth ? "w-full" : null,
+        { "scale-[0.95]": isMouseDown },
         className
     );
 
+    const onMouseDownHandler = () => setIsMouseDown(true);
+
+    const onMouseUpHandler = () => setIsMouseDown(false);
+
     return (
-        <button className={classes} {...rest}>
+        <button
+            className={classes}
+            onMouseDown={onMouseDownHandler}
+            onMouseUp={onMouseUpHandler}
+            onMouseOut={onMouseUpHandler}
+            {...rest}
+        >
             {variant === "expand-small" && <ChevronRight width="15px" />}
             {variant === "expand-medium" && <ChevronRight width="18px" />}
 
