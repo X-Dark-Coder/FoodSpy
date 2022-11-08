2028eb20f1e877cd50598ede7f6701df711a8da8
Mansour <84951627+marzban-dev@users.noreply.github.com>
2022-11-08 20:59:20 +0330

    Fix logo

--- src/layouts/AppLayout/components/SideMenu/side-menu.tsx
+++ src/layouts/AppLayout/components/SideMenu/side-menu.tsx
@@ -1,5 +1,5 @@
 import { Navbar, Profile } from "components";
-import { ReactComponent as Logo } from "assets/img/Brand Logo.svg";
+import Logo from "assets/img/logo.png";
 
 const SideMenu: React.FC = () => {
     return (
@@ -7,7 +7,7 @@ const SideMenu: React.FC = () => {
             className="hidden md:flex flex-col justify-between items-start w-[230px] h-screen bg-white shadow-[4px_0_24px_0_#00000008]">
             <div className="w-full">
                 <div className="px-3 pt-[10px] pb-14">
-                    <Logo className="w-[calc(100%_-_20px)] h-[36px]" />
+                    <img src={Logo} alt="logo" className="w-[calc(100%_-_30px)]"/>
                 </div>
 
                 <Navbar />
